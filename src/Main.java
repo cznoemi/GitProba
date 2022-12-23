7badb68747dc82e25a9dc9bd6c783d5284ac19aa
cznoemi <cznoncsy@gmail.com>
2022-12-23 13:28:55 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import Races.Human;
+import Races.Orc;
+
+public class Main {
+    public static void main(String[] args){
+        Human human = new Human("Human");
+        Orc orc = new Orc("Orc");
+    }
+
+}
